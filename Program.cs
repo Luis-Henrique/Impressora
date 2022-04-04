348b956c758d0c442594a611f68ff1ba45acdcb6
Luis Henrique <henriqueluis3104@hotmail.com>
2022-04-04 12:46:30 -0300

    Implementação da classe Xerox

--- Program.cs
+++ Program.cs
@@ -9,5 +9,12 @@ class Program
 
         var c = new ImpresoraCopiadora();
         Console.WriteLine(c.Copiar("Estou estudando C#"));
+
+        CopiarDocumento(new Xerox(), "Estou estudando C#");
+    }
+
+    public static void CopiarDocumento(Icopiadora c, string texto)
+    {
+        Console.WriteLine("Estou copiando o texto a seguir:" + c.Copiar(texto));
     }
 }
---
+++ Xerox.cs
@@ -0,0 +1,8 @@
+
+public class Xerox : Icopiadora
+{
+    public string Copiar(string texto)
+    {
+        return $"Texto a copiar: {texto}";
+    }
+}
